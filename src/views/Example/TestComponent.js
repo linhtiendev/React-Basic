d70b1b0a9250c74866311425f5344c2d865fd4db
linhtiendev <lelinhtien0707@gmail.com>
2023-08-18 11:57:44 +0700

    create and use state

--- src/views/Example/TestComponent.js
+++ src/views/Example/TestComponent.js
@@ -2,9 +2,38 @@ import React from "react";
 
 // class TestComponent kế thừa React.Component
 class TestComponent extends React.Component {
+    state = {
+        name: 'Liti ne',
+        age: '22'
+    };
+    // -> có 2 cách in ra state
+    /*
+    JSX => return block <=> trả về 1 phần tử html
+    -> dùng {} khi muốn dùng js bên trong html
+    -> nếu không muốn bọc 1 div ở ngoài thì dùng fragment <=> <></>
+    // this chính là bản thân class, dùng dấu . để tham chiếu đến 1 method
+    */
+
+    handleOnchangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
     render() {
+        // let name = 'Liti'
         return (
-            <div>TestComponent</div>
+            <>
+                <div>
+                    {/* nhập dữ liệu realtime */}
+                    <input value={this.state.name} type="text" 
+                        onChange={(event) => this.handleOnchangeName(event)}
+                    /> <br/>
+                    Hello world, my name is { this.state.name }
+                </div>
+                <div>
+                    Im {this.state['age']} years old
+                </div>
+            </>
         )
     }
 }
