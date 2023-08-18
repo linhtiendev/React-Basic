6eeec1a92072b950425a39af3b349bef45b227f6
linhtiendev <lelinhtien0707@gmail.com>
2023-08-18 22:25:29 +0700

    handle events in form html

--- src/views/Example/TestComponent.js
+++ src/views/Example/TestComponent.js
@@ -1,63 +1,62 @@
 import React from "react";
 
-// class TestComponent kế thừa React.Component
-class TestComponent extends React.Component {
-    state = {
-        name: '', // khởi tạo giá trị rỗng
-        age: ''
-    };
-    // -> có 2 cách in ra state
-    /*
-    JSX => return block <=> trả về 1 phần tử html
-    -> dùng {} khi muốn dùng js bên trong html
-    -> nếu không muốn bọc 1 div ở ngoài thì dùng fragment <=> <></>
-    // this chính là bản thân class, dùng dấu . để tham chiếu đến 1 method
-    */
 
-    // event => gọi đến object của html
-    // target => trả ra 1 element của html
-    // value => gọi đến thuộc tính value của html
-    handleOnchangeName = (event) => {
-        this.setState({
-            name: event.target.value, // nhập giá trị thì state sẽ được cập nhật
-            age: 'snake'
-            // event.target sẽ trả ra 1 biến input -> gọi đến thuộc tính value của nó
-            // -> sẽ lấy được giá trị của input hiện tại
-            // lấy được các biến hiện tại
-            // muốn cập nhật lại state thì dùng setState()
+class TestComponent extends React.Component {
+
+    // khởi tạo state rỗng
+    state = {
+        firstName: "",
+        lastName: ""
+    }
+
+    // lưu state đã được thay đổi
+    handleChangeFirstName = (event) => {
+        this.setState({ 
+            firstName: event.target.value 
+        });
+    }
+    handleChangeLastName = (event) => {
+        this.setState({ 
+            lastName: event.target.value
         })
     }
+    handleSubmit = (event) => {
+        event.preventDefault(); // hàm không tải lại website
+        console.log("check data input", this.state);
 
-    handleOnClickAlert = () => {
-        alert("Hellooooo")
     }
-    // khi thay đổi input react -> set lại state -> render -> re-render 
+    /*
+    -> tạo 1 form để lưu fname lname trong input 
+    -> mỗi lần onChange đẫ lưu lại
+    -> khi click submit thì nó lấy data đó truyền lên API
+    */
     render() {
-        // let name = 'Liti'
         return (
             <>
-                <div>
-                    {/* nhập dữ liệu realtime 
-                        -> event trong onChange là event của html DOM
-                        -> nhờ event mới có thể lấy được giá trị
-                    */}
-                    <input value={this.state.name} type="text" 
-                        onChange={(event) => this.handleOnchangeName(event)}
-                    /> <br/>
-                    Hello world, my name is { this.state.name }
-                </div>
-                <div>
-                    Im {this.state['age']} years old
-                </div>
-                <div className="third">
-                    <button onClick={() => this.handleOnClickAlert()}>Click me</button>
-                </div>
+                <form>
+                    <label htmlFor="fname">First name:</label><br/>
+                    <input 
+                        type="text" 
+                        value={this.state.firstName}
+                        onChange={(event) => this.handleChangeFirstName(event)}
+                    />
+                    <br/>
+                    <label htmlFor="lname">Last name:</label><br/>
+                    <input 
+                        type="text" 
+                        value={this.state.lastName} // muốn giá trị cập nhật realtime
+                        onChange={(event) => this.handleChangeLastName(event)}
+                    />
+                    <br/>
+                    <br/>
+                    <input 
+                        type="submit" 
+                        value="Submit"
+                        onClick={(event) => this.handleSubmit(event)}
+                    />
+                </form> 
             </>
         )
     }
 }
-
-// muốn sử dụng component ở file khác thì phải export
-// export default -> trả ra duy nhất 1 class
-// export {} -> trả ra nhiều class bên trong object
 export default TestComponent;
