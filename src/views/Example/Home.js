1517bc4d2a9d7e9bdf8fdb2af3c7127a26a3945c
linhtiendev <lelinhtien0707@gmail.com>
2023-08-21 15:15:53 +0700

    redirect with reactjs routers

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 class Home extends React.Component {
+    //Redirect page
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.history.push("/todo");
+        }, 4000);
+    }
+
     render() {
         return <div>Home Component</div>;
     }
 }
-export default Home;
+export default withRouter(Home);
