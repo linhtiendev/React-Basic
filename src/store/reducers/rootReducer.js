75018a98d1a7490799254b29066427dbe754b0ed
linhtiendev <lelinhtien0707@gmail.com>
2023-08-22 21:58:17 +0700

    use redux to CREATE/DELETE data react

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -9,9 +9,27 @@ const initState = {
         { id: 1, name: "LiTi" },
         { id: 2, name: "MiNi" },
     ],
+    posts: [],
 };
 // khi khởi tạo thì biến state đã có giá trị là initState
 const rootReducer = (state = initState, action) => {
-    return state;
+    switch (action.type) {
+        case "DELETE_USER":
+            let users = state.users;
+            users = users.filter((item) => item.id !== action.payload.id);
+            return {
+                ...state,
+                users,
+            };
+        case "CREATE_USER":
+            let id = Math.floor(Math.random() * 10000);
+            let user = { id: id, name: `Random - ${id}` };
+            return {
+                ...state,
+                users: [...state.users, user],
+            };
+        default:
+            return state;
+    }
 };
 export default rootReducer;
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import banner from "../../assets/images/bg-test-02.jpg";
-import { Connect } from "react-redux";
+import { connect } from "react-redux";
 
 class Home extends React.Component {
     //Redirect page
@@ -11,12 +11,50 @@ class Home extends React.Component {
         // }, 4000);
     }
 
+    handleDeleteUser = (user) => {
+        console.log("check delete", user);
+        this.props.deleteUserRedux(user);
+    };
+
+    handleCreateUser = () => {
+        this.props.addUserRedux();
+    };
+
     render() {
+        // console.log(this.props.dataRedux);
+        // Tạo biến hứng state redux
+        let listUsers = this.props.dataRedux;
         return (
             <>
                 <div>Component Home</div>
                 <div>
                     <img src={banner} />
+                </div>
+                <div>
+                    {/* đk in state redux */}
+                    {listUsers &&
+                        listUsers.length > 0 &&
+                        listUsers.map((item, index) => {
+                            return (
+                                <div key={item.id}>
+                                    {index + 1}. {item.name} &nbsp;
+                                    <button
+                                        type="button"
+                                        onClick={() =>
+                                            this.handleDeleteUser(item)
+                                        }
+                                    >
+                                        x
+                                    </button>
+                                    <button
+                                        type="button"
+                                        onClick={() => this.handleCreateUser()}
+                                    >
+                                        Add new
+                                    </button>
+                                </div>
+                            );
+                        })}
                 </div>
             </>
         );
@@ -28,6 +66,18 @@ class Home extends React.Component {
 const mapStateToProps = (state) => {
     return { dataRedux: state.users };
 };
+
+// 1 action cuar redux là 1 object gồm có
+// type: định nghĩa tên của action
+// payload: dữ liệu muốn mang theo action đấy
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteUserRedux: (userDelete) =>
+            dispatch({ type: "DELETE_USER", payload: userDelete }),
+        addUserRedux: () => dispatch({ type: "CREATE_USER" }),
+    };
+};
+
 // Dùng hàm Connect giúp dứng dụng react và redux liên kết với nhau
 // và chỉ liên kết trong component được gọi
-export default Connect(mapStateToProps)(withRouter(Home));
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Home));
